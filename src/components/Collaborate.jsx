b0c309071f17d5b9fafe54f307f9eef2efbc07c8
MD MAHFUZAR RAHMAN TAREK <mahfuzar175@gmail.com>
2024-03-04 19:32:22 +0600

    cards added to the collaborate section

--- src/components/Collaborate.jsx
+++ src/components/Collaborate.jsx
@@ -1,5 +1,5 @@
 import highlight from "../assets/images/idea.png";
-import { LuMoveRight } from "react-icons/lu";
+
 const Collaborate = () => {
   return (
     <div className="py-6 md:py-7 overflow-hidden relative lg:py-12 mt-10 font-inter">
@@ -10,6 +10,35 @@ const Collaborate = () => {
         </h1>
         <div className="absolute lg:block -top-4 md:-top-6 left-12 md:left-[58%]">
           <img src={highlight} alt="highlite" />
+        </div>
+      </div>
+      {/* card section */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4 md:mt-5 lg:mt-6">
+        <div className="border border-white p-4">
+          <h1 className="text-[#050038] text-xl font-bold">Free forever</h1>
+          <p className="text-slate-600 pt-3 lg:pt-4">
+            Our free plan gives you unlimited team members, 3 boards, and 300+
+            expert-made templates. Signing up with your work email lets you
+            bring in your team faster. See our <br /> <span className="text-[#4262FF]">pricing plans</span> for more features.
+          </p>
+        </div>
+        <div className="border border-white p-4">
+          <h1 className="text-[#050038] text-xl font-bold">
+            Easy integrations
+          </h1>
+          <p className="text-slate-600 pt-3 lg:pt-4">
+            Miro has 100+ powerful integrations with tools you already use like
+            G <br /> Suite, Slack, and Jira, so your <br /> workflow is seamless. View the
+            full <br /> list in our <span className="text-[#4262FF]">Marketplace</span> .
+          </p>
+        </div>
+        <div className="border border-white p-4">
+          <h1 className="text-[#050038] text-xl font-bold">Security first</h1>
+          <p className="text-slate-600 pt-3 lg:pt-4">
+            We treat your data like you would — with the utmost care. We follow
+            industry-leading security standards <br /> and give you tools to protect
+            intellectual property. Learn more <br /> at our <span className="text-[#4262FF]">Trust Center</span> .
+          </p>
         </div>
       </div>
     </div>

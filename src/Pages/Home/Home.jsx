79dc9d42456fc1acc23f49b168265fdb1d9c312b
MD MAHFUZAR RAHMAN TAREK <mahfuzar175@gmail.com>
2024-03-04 19:38:06 +0600

    team work section created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../../components/Banner";
 import Brand from "../../components/Brand";
 import Collaborate from "../../components/Collaborate";
+import TeamWork from "../../components/TeamWork";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
         <Banner></Banner>
         <Brand></Brand>
         <Collaborate></Collaborate>
+        <TeamWork></TeamWork>
     </div>
   );
 };
---
+++ src/components/TeamWork.jsx
@@ -0,0 +1,9 @@
+const TeamWork = () => {
+    return (
+        <div>
+            <h2>Team Work Section</h2>
+        </div>
+    );
+};
+
+export default TeamWork;

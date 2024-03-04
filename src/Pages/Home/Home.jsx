4b94f7a4d849b7be7ded77be991392b2991b9f10
MD MAHFUZAR RAHMAN TAREK <mahfuzar175@gmail.com>
2024-03-04 15:14:42 +0600

    brand section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../../components/Banner";
+import Brand from "../../components/Brand";
 
 const Home = () => {
   return (
     <div>
         <Banner></Banner>
+        <Brand></Brand>
     </div>
   );
 };
--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,10 +1,24 @@
 import Marquee from "react-fast-marquee";
+import brand1 from '../assets/images/Walmart.svg'
+import brand2 from '../assets/images/cisco.svg.svg'
+import brand3 from '../assets/images/volvo.svg'
+import brand4 from '../assets/images/deloitte.svg.svg'
+import brand5 from '../assets/images/okta.svg.svg'
 const Brand = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="py-3 md:py-7 overflow-hidden lg:py-16">
+        <h1 className="text-[18px] mb-10 font-normal text-slate-500 text-center">
+        Trusted by 45M+ users
+      </h1>
+      <Marquee>
+        <img className="mr-9" src={brand1} alt="brand" />
+        <img className="mr-9" src={brand2} alt="brand" />
+        <img className="mr-9" src={brand3} alt="brand" />
+        <img className="mr-9" src={brand4} alt="brand" />
+        <img src={brand5} alt="brand" />
+      </Marquee>
+    </div>
+  );
 };
 
 export default Brand;

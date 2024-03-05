50d6d5a8bbc65eccacda881f981defd20341c2a6
MD MAHFUZAR RAHMAN TAREK <mahfuzar175@gmail.com>
2024-03-05 20:31:05 +0600

    light theme added

--- src/components/WorkWay.jsx
+++ src/components/WorkWay.jsx
@@ -1,8 +1,8 @@
 import Work from '../assets/images/Work_Way.png'
 const WorkWay = () => {
     return (
-        <div className='overflow-hidden py-6 relative'>
-            <img src={Work} alt="Work_Way" />
+        <div className='overflow-hidden py-6 flex justify-center items-center'>
+            <img className='w-full' src={Work} alt="Work_Way" />
         </div>
     );
 };
